ac8c888ef5a92eed4ccb4a3ae8a0afda1041a9b7
Jonathan Mann <mann17@gmail.com>
2015-02-22 10:01:45 -0500

    pass integers to tuples

--- relational_model.hs
+++ relational_model.hs
@@ -1,17 +1,6 @@
---cabal install split
---cabal install uuid
-
---used
 import System.Environment
 import Data.List.Split
 import Data.String.Utils
-
---not used
-import Data.List
-import Data.Maybe
-import Data.UUID as U
-import qualified Data.UUID.V4 as U4
-import Control.Monad
 
 readData :: FilePath -> IO (String)
 readData file = do
@@ -19,19 +8,16 @@ readData file = do
     return content
 
 processData :: String -> String
-processData content = cleanData (unlines([ processLine(line) | line <- lines(content)]))
+processData content = cleanData (unlines([ processLine line 1 | line <- lines(content)]))
 
 cleanData :: String -> String
 cleanData content = replace "\r" "" content
 
---U.toString U4.nextRandom
-processLine :: String -> String
-processLine line =  unwords [ (getTuple col 3 1) | col <- (splitOn "," line)]
---processLine line =  unwords [[ "(" ++ ("1") ++ "," ++ (col) ++ ")" | col <- (splitOn "," line)]!!1] 
+processLine :: String -> Int -> String
+processLine line r_ord =  unwords [ (getTuple col r_ord 1) | col <- (splitOn "," line)]
 
 getTuple :: String -> Int -> Int -> String
 getTuple col r_ord c_ord  = "(" ++ (show r_ord) ++ "," ++ (show c_ord) ++ "," ++ (col) ++ ")"
-
 
 outputData :: String -> IO ()
 outputData contents = writeFile "relations.log" contents
