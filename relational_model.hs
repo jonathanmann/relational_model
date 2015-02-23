fe3ea3beeed22c35ffada867265515bcacd1514e
Jonathan Mann <mann17@gmail.com>
2015-02-22 20:24:22 -0500

    create real tuples

--- relational_model.hs
+++ relational_model.hs
@@ -1,6 +1,7 @@
 import System.Environment
 import Data.List.Split
 import Data.String.Utils
+import Data.List
 
 readData :: FilePath -> IO (String)
 readData file = do
@@ -17,20 +18,19 @@ cleanData :: String -> String
 cleanData content = replace "\r" "" content
 
 processData :: String -> String
-processData content = unlines([ prepLine line (head (lines(content))) | line <- (zip[0..] (lines(content)) )])
+processData content = unlines([ show (prepLine line) | line <- (zip[0..] (lines(content)) )])
 
-prepLine :: (Int,String) -> String -> String
-prepLine line headers = processLine (snd line) (fst line) (spl headers)
+prepLine :: (Int,String) -> [(Int,String)]
+prepLine line = processLine (snd line) (fst line)
 
-processLine :: String -> Int -> [String] -> String
-processLine line r_ord headers =  unwords [ (getTuple (snd col) r_ord (fst col)) | col <- (zip headers (spl line))]
+processLine :: String -> Int -> [(Int,String)]
+processLine line r_ord = [ (getTuple col r_ord) | col <- (spl line)]
 
-getTuple :: String -> Int -> String -> String
-getTuple col r_ord c_name  = "(" ++ (c_name) ++ "," ++ (show r_ord) ++ "," ++ (col) ++ ")"
+getTuple :: String -> Int -> (Int,String) 
+getTuple col r_ord = (r_ord,col)
 
 main :: IO ()
 main = do
     file:_ <- getArgs
     content <- readData file
     outputData $ processData (cleanData content)
-    --putStrLn "check relations.log"
